6e69acc0dfe78ff3a4e7c72c9f0b9047374124e2
Timothy Martin <timothymartin76@users.noreply.github.com>
2016-01-20 21:42:28 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ var map;
     map = new L.Map('map', { 
 	zoomControl: false,
       center: [40.694526,-73.936176], 
-      zoom: 11,
+      zoom: 12,
     })
    var layer = L.tileLayer('',{
   attribution: ''
